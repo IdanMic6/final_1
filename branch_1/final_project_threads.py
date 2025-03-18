b2277882934998198fcf0c29854cae01a5ac839f
Idan Mic <idan.mic6@gmail.com>
2025-03-18 11:11:51 +0200

    Update submodule content

--- branch_1/final_project_threads.py
+++ branch_1/final_project_threads.py
@@ -1,14 +1,14 @@
 import threading
 import requests
  
-total_chars = 0
+TOTAL_CHARS = 0
 
 def downloader(url, thread_id):
-    global total_chars
+    global TOTAL_CHARS
     response = requests.get(url).json()  
     response_str = str(response)
     num_chars = len(response_str)
-    total_chars += num_chars
+    TOTAL_CHARS += num_chars
 
     print("Thread " + str(thread_id) + " downloaded " + str(num_chars) + " chars from " + url)
     
@@ -33,7 +33,7 @@ def main():
     for thread in threads:
         thread.join()
 
-    print("Total number of chars downloaded is: " + str(total_chars))
+    print("Total number of chars downloaded is: " + str(TOTAL_CHARS))
 
 if __name__ == "__main__":
     main()
